416138ff803627f47b029b1a4eaa4858366b74e5
Nick Kuijpers <info@niku-solutions.nl>
2018-01-10 16:57:32 +0100

    Fixed wierd bug when using 'id' as route param, not reading strings. Fixed to identifier now

--- src/Cms.php
+++ src/Cms.php
@@ -66,15 +66,15 @@ class Cms
 
 			// Crud listing all posts by post type
 			Route::post('/{post_type}', '\Niku\Cms\Http\Controllers\Cms\ListPostsController@init')->name('list');
-			Route::post('/{post_type}/show/{id}', '\Niku\Cms\Http\Controllers\Cms\ShowPostController@init')->name('show');
+			Route::post('/{post_type}/show/{identifier}', '\Niku\Cms\Http\Controllers\Cms\ShowPostController@init')->name('show');
 
 			// Taxonomy
-			Route::post('/{post_type}/show/{id}/taxonomies/{sub_post_type}', '\Niku\Cms\Http\Controllers\Cms\ShowPostTaxonomies@init')->name('show');
-			Route::post('/{post_type}/show/{id}/posts/{sub_post_type}', '\Niku\Cms\Http\Controllers\Cms\ShowTaxonomyPosts@init')->name('show');
+			Route::post('/{post_type}/show/{identifier}/taxonomies/{sub_post_type}', '\Niku\Cms\Http\Controllers\Cms\ShowPostTaxonomies@init')->name('show');
+			Route::post('/{post_type}/show/{identifier}/posts/{sub_post_type}', '\Niku\Cms\Http\Controllers\Cms\ShowTaxonomyPosts@init')->name('show');
 
 			// Crud
-			Route::post('/{post_type}/delete/{id}', '\Niku\Cms\Http\Controllers\Cms\DeletePostController@init')->name('delete');
-			Route::post('/{post_type}/edit/{id}', '\Niku\Cms\Http\Controllers\Cms\EditPostController@init')->name('edit');
+			Route::post('/{post_type}/delete/{identifier}', '\Niku\Cms\Http\Controllers\Cms\DeletePostController@init')->name('delete');
+			Route::post('/{post_type}/edit/{identifier}', '\Niku\Cms\Http\Controllers\Cms\EditPostController@init')->name('edit');
 			Route::post('/{post_type}/create', '\Niku\Cms\Http\Controllers\Cms\CreatePostController@init')->name('create');
 		});
     }
