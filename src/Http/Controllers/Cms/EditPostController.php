0c08acdbcf4db1ef94dce4a2f9119e28345a86b5
Nick Kuijpers <info@niku-solutions.nl>
2018-02-21 19:46:43 +0000

    added config to edit post

--- src/Http/Controllers/Cms/EditPostController.php
+++ src/Http/Controllers/Cms/EditPostController.php
@@ -119,8 +119,11 @@ class EditPostController extends CmsController
 			$successMessage = $postTypeModel->successMessage['post_updated'];
 		}
 
+		$config = $this->getConfig($postTypeModel);
+
         // Lets return the response
     	return response()->json([
+			'config' => $config,
     		'code' => 'success',
     		'message' => $successMessage,
     		'action' => 'edit',
@@ -134,7 +137,75 @@ class EditPostController extends CmsController
 				'updated_at' => $post->updated_at,
     		],
     	], 200);
-    }
+	}
+	
+	public function getConfig($postTypeModel)
+	{
+		// Merge the configuration values
+		$config = [];
+		if($postTypeModel->config){
+			$config = $postTypeModel->config;
+		}
+
+        $config = $config;
+
+        // Adding public config
+        if($postTypeModel->skipCreation){
+			$config['skip_creation'] = $postTypeModel->skipCreation;
+			if($postTypeModel->skipToRouteName){
+				$config['skip_to_route_name'] = $postTypeModel->skipToRouteName;
+			}
+        } else {
+			$config['skip_creation'] = false;
+			$config['skip_to_route_name'] = '';
+		}
+		
+		// Adding public config
+        if($postTypeModel->disableEditOnlyCheck){
+        	$config['disable_edit_only_check'] = $postTypeModel->disableEditOnlyCheck;
+        } else {
+        	$config['disable_edit_only_check'] = false;
+		}
+
+		if($postTypeModel->disableEdit){
+        	$config['disable_edit'] = $postTypeModel->disableEdit;
+        } else {
+        	$config['disable_edit'] = false;
+		}
+
+		if($postTypeModel->disableDelete){
+        	$config['disable_delete'] = $postTypeModel->disableDelete;
+        } else {
+        	$config['disable_delete'] = false;
+		}
+
+		if($postTypeModel->disableCreate){
+        	$config['disable_create'] = $postTypeModel->disableCreate;
+        } else {
+        	$config['disable_create'] = false;
+		}
+		
+		if($postTypeModel->getPostByPostName){
+        	$config['get_post_by_postname'] = $postTypeModel->getPostByPostName;
+        } else {
+        	$config['get_post_by_postname'] = false;
+		}
+
+		$allKeys = collect($this->getValidationsKeys($postTypeModel));
+
+		// Adding public config
+        if($postTypeModel->enableAllSpecificFieldsUpdate){
+        	$config['specific_fields']['enable_all'] = $postTypeModel->enableAllSpecificFieldsUpdate;
+			$config['specific_fields']['exclude_fields'] = $postTypeModel->excludeSpecificFieldsFromUpdate;			
+			$config['specific_fields']['enabled_fields'] = $allKeys->keys();
+        } else {
+        	$config['specific_fields']['enable_all'] = $postTypeModel->enableAllSpecificFieldsUpdate;
+			$config['specific_fields']['exclude_fields'] = $postTypeModel->excludeSpecificFieldsFromUpdate;			
+			$config['specific_fields']['enabled_fields'] = $allKeys->where('single_field_updateable.active', 'true')->keys();
+		}
+
+		return $config;
+	}
 
     /**
      * Validating the creation and change of a post
