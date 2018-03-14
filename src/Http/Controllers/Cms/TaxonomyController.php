55816e022a3151e1efb9fe476b5d55cea04662bc
Nick Kuijpers <info@niku-solutions.nl>
2018-03-14 18:40:57 +0100

    work in progress adding flexibility

--- src/Http/Controllers/Cms/TaxonomyController.php
+++ src/Http/Controllers/Cms/TaxonomyController.php
@@ -34,15 +34,6 @@ class TaxonomyController extends CmsController
     		return $this->abort($errorMessages);
 		}
 
-		// // Disable editting of form
-		// if($postTypeModel->disableEditOnlyCheck){
-        // 	$errorMessages = 'The post type does not support editting.';
-    	// 	if(array_has($postTypeModel->errorMessages, 'post_type_identifier_does_not_support_edit')){
-    	// 		$errorMessages = $postTypeModel->errorMessages['post_type_identifier_does_not_support_edit'];
-    	// 	}
-    	// 	return $this->abort($errorMessages);
-		// }
-
 		// Disable editting of form
 		if($postTypeModel->disableEdit){
         	$errorMessages = 'The post type does not support editting.';
@@ -141,56 +132,7 @@ class TaxonomyController extends CmsController
     		'code' => 'success',
     		'message' => $successMessage,
     		'action' => 'taxonomy_edit',
-    		// 'taxonomy' => [
-    		// 	'id' => $taxonomyInstance->id,
-    		// 	'post_id' => $taxonomyInstance->post_id,
-    		// 	'taxonomy_post_id' => $taxonomyInstance->taxonomy_post_id,
-			// 	'custom' => $taxonomyInstance->custom,
-    		// ],
     	], 200);
-    }
-
-    /**
-     * Validating the creation and change of a post
-     */
-    protected function validatePost($request, $post, $validationRules)
-    {
-    	$validationRules = $this->validateFieldByConditionalLogic($validationRules, $post, $post);
-
-    	// Lets receive the current items from the post type validation array
-    	if(array_key_exists('post_name', $validationRules) && !is_array($validationRules['post_name'])){
-
-	    	$exploded = explode('|', $validationRules['post_name']);
-
-	    	$validationRules['post_name'] = [];
-
-	    	foreach($exploded as $key => $value){
-	    		$validationRules['post_name'][] = $value;
-	    	}
-		}
-
-    	// Lets validate if a post_name is required.
-        if(!$post->disableDefaultPostName){
-
-			// If we are edditing the current existing post, we must remove the unique check
-			if($request->get('post_name') == $post->post_name){
-
-		    	$validationRules['post_name'] = 'required';
-
-		    // If this is not a existing post name, we need to validate if its unique. They are changing the post name.
-		    } else {
-
-		    	// Make sure that only the post_name of the requested post_type is unique
-		        $validationRules['post_name'][] = 'required';
-		        $validationRules['post_name'][] = Rule::unique('cms_posts')->where(function ($query) use ($post) {
-				    return $query->where('post_type', $post->identifier);
-				});
-
-		    }
-
-		}
-
-        return $this->validate($request, $validationRules);
     }
 
 }
